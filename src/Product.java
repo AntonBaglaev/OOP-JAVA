f2db9d35c9db0a87cdaf9ab2f8fdb323b50f1b0c
Anton Baglaev <abaglaev@yandex.ru>
2024-12-05 15:30:58 +0300

    initinal and seminar1

---
+++ src/Product.java
@@ -0,0 +1,33 @@
+public abstract class Product {
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    private String name;
+    private int price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
